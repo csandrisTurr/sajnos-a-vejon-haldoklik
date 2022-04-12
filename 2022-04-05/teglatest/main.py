3cf3b183d00b637182eb11fe51f2e3217c231507
csandrisTurr <csaszarandras@turr.hu>
2022-04-12 08:20:38 +0200

    henger számítások extra feladat

--- 2022-04-05/teglatest/main.py
+++ 2022-04-05/teglatest/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
+from math import pi
 
 root = Tk()
 glob_sz_typ: "felszin" or "terfogat" = None
@@ -75,6 +76,55 @@ def szamit():
 
     szamitas_ablak.mainloop()
 
+def szamit_henger():
+    
+    global glob_sz_typ
+    glob_sz_typ = glob_sz_typ
+    szamitas_ablak = Toplevel(root)
+
+    def display_result():
+        
+        try: # Trying to convert into int
+            r = int(r_entry.get())
+            m = int(m_entry.get())
+        except:
+            return messagebox.showerror("Hiba", "Valahol nem számot adtál meg paraméternek!")
+        
+        if r <= 0 or m <= 0:
+            return messagebox.showerror("Hiba", "Kérlek a pontos számítás érdekében olyan számokat adj meg, amik nem mondanak ellent a fizika törvényeinek")
+
+        eremeny = 2*r*pi*(r+m) if glob_sz_typ == "felszin" else pow(r, 2)*pi*m
+
+        eredmeny_label.config(text = eredmeny_def_text.format(round(eremeny, 2)))
+
+    # DEFINING VARS
+
+    eredmeny_def_text = "Eredmeny: {}"
+
+    r_label = Label(szamitas_ablak, text = "r (cm) : ")
+    m_label = Label(szamitas_ablak, text = "m (cm) : ")
+    eredmeny_label = Label(szamitas_ablak, text = eredmeny_def_text.format("-"))
+
+    r_entry = Entry(szamitas_ablak)
+    m_entry = Entry(szamitas_ablak)
+
+    kisz_b = Button(szamitas_ablak, text = "Számítás", command = display_result)
+    kil_b = Button(szamitas_ablak, text = "Kilép", command = szamitas_ablak.destroy)
+
+    
+    r_label.grid(row = 0, column = 0)
+    r_entry.grid(row = 0, column = 1)
+
+    m_label.grid(row = 1, column = 0)
+    m_entry.grid(row = 1, column = 1)
+
+    kisz_b.grid(row = 3, column = 0)
+    kil_b.grid(row = 5, column = 0)
+    
+    eredmeny_label.grid(row = 4, column = 0)
+
+    szamitas_ablak.mainloop()
+
 def terfogat():
     global glob_sz_typ
     glob_sz_typ = "terfogat"
@@ -85,12 +135,21 @@ def felszin():
     glob_sz_typ = "felszin"
     szamit()
 
+def h_terfogat():
+    global glob_sz_typ
+    glob_sz_typ = "felszin"
+    szamit_henger()
+
+def h_felszin():
+    global glob_sz_typ
+    glob_sz_typ = "felszin"
+    szamit_henger()
+
 # Defining
 menusor = Frame(root)
 menusor.pack(side = LEFT)
 
 menu1 = Menubutton(menusor, text = "File", underline = 0)
-menu1.pack(side = LEFT)
 
 fajl = Menu(menu1)
 fajl.add_command(label = "Névjegy", command = nevjegy)
@@ -98,15 +157,23 @@ fajl.add_command(label = "Kilépés", command = root.destroy)
 menu1.config(menu = fajl)
 
 menu2 = Menubutton(menusor, text = "Téglatest", underline = 0)
-menu2.pack(side = LEFT)
 
 teglatest = Menu(menu2)
 teglatest.add_command(label = "Felszín", command = felszin)
 teglatest.add_command(label = "Térfogat", command = terfogat)
 menu2.config(menu = teglatest)
 
-# Placement
+menu3 = Menubutton(menusor, text = "Henger", underline = 0)
 
+henger_m = Menu(menu3)
+henger_m.add_command(label = "Felszín", command = h_felszin)
+henger_m.add_command(label = "Térfogat", command = h_terfogat)
+menu3.config(menu = henger_m)
+
+# Placement
+menu1.pack(side = LEFT)
+menu2.pack(side = LEFT)
+menu3.pack(side = LEFT)
 
 # Launch
 root.mainloop()
